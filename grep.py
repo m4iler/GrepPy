48904ef7efb5712f4949aac88e0a7007ddb8f16f
m4iler <m4iler@cock.li>
2018-03-15 17:30:58 +0100

    hell

--- grep.py
+++ grep.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-
-
 """
 USAGE: grep.py PATTERN FILE
 Finds string in file (similar to grep)
@@ -13,26 +11,31 @@ try:
     import regex as re
 except ModuleNotFoundError:
     import re
-    if "-v" in sys.argv[1:]:
-        print("You don't have RegEx! Using the RE module.", file=sys.stderr)
+    print("You don't have RegEx! Using the RE module.", file=sys.stderr)
+
+
+def grep(f, term):
+    for line in f:
+        if re.search(term, line):
+            print(line, end="")
+
 
 def get_args(argv):
-    try:
-        term, path = argv
-        return term, path
-    except ValueError:
+    if len(argv) < 2:
         print(__doc__.strip(), file=sys.stderr)
 
+    return argv[0], argv[1:]
+
+
 def main(argv):
-    term, path = get_args(argv)
-    try:
-        with open(path) as f:
-            for line in f:
-                if re.search(term, line):
-                    print(line, end="")
-    except FileNotFoundError:
-        print("No file found", file = sys.stderr)
-        sys.exit(1)
+    term, paths = get_args(argv)
+    for path in paths:
+        try:
+            with open(path) as f:
+                grep(f, term)
+        except FileNotFoundError:
+            print("No file found", file=sys.stderr)
+            sys.exit(1)
 
 
 if __name__ == "__main__":
